fef85533799d6f99aac7ad4f71fb3ab92c5016a8
pattipatijahnavi <p.jahnavi66@gmail.com>
2020-01-03 14:42:26 +0530

    4 files modified, 11 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { AboutComponent } from './about/about.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [ BrowserModule, FormsModule,NgbModule, RouterModule.forRoot([
+    {path:'content', component:ContentComponent},
+    {path:'about', component:AboutComponent},
+    {path:'overview', component:OverviewComponent}
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, NavComponent, ContentComponent, AboutComponent, OverviewComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
